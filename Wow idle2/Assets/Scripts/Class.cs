2f7e3b775413c0efe2fd7cac25a914685ac3143b
madsi2000 <madsi2000@gmail.com>
2018-10-08 10:24:29 +0200

    creat projekt

---
+++ Wow idle2/Assets/Scripts/Class.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+
+public enum Classes { mage, warrior, rogue }
+
+public class Class : Values {
+
+
+    public System.Random rnd = new System.Random();
+    Values val;
+
+    void Start () {
+
+        Classes c = (Classes)(rnd.Next(0, 3));
+        switch (c)
+        {
+            case Classes.mage:
+                itemStatsInt(0);
+                break;
+
+            case Classes.warrior:
+                itemStatsStr(0);
+                break;
+            case Classes.rogue:
+                itemStatsAgi(0);
+                break;
+        }
+    {
+
+    }		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void itemStatsInt(int x)
+    {
+        val.Intellect = x;
+    }
+
+    public void itemStatsStr(int x)
+    {
+        val.strength = x;
+    }
+
+    public void itemStatsAgi(int x)
+    {
+        val.agility = x;
+    }
+}
---
+++ Wow idle2/Assets/Scripts/Values.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+
+public class Values : MonoBehaviour {
+
+
+
+
+    public int strength;
+    private int intellect;
+    public int agility;
+
+    public int attackPower;
+    public int spellPower;
+    public int wepDmg;
+
+    public int Intellect
+    {
+        get
+        {
+            return intellect;
+        }
+
+        set
+        {
+            intellect = value;
+        }
+    }
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+   
+
+    }
+}
